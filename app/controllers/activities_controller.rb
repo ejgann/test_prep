73b86215caf220d23f711b1257a8a65a53b28c56
Emily Gann <aspentree79@gmail.com>
2020-08-09 11:25:47 -0400

    build activities_controller

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,2 +1,48 @@
 class ActivitiesController < ApplicationController
+
+    def index
+        @activities = Activity.all
+    end
+
+    def new
+        @activity = Activity.new
+    end
+
+    def create
+        @activity = Activity.new(activity_params)
+        if @activity.save
+            redirect_to activities_path
+        else
+            render :new
+        end
+    end
+
+    def show
+        @activity = Activity.find_by(id: params[:id])
+    end
+
+    def edit
+        @activity = Activity.find_by(id: params[:id])
+    end
+    
+    def update
+        @activity = Activity.find_by(id: params[:id])
+        if @activity.update(activity_params)
+            redirect to activity_path(@activity)
+        else
+            render :edit
+        end
+    end
+
+    def destroy
+        @activity = Activity.find_by(id: params[:id])
+        @activity.destroy 
+        redirect_to activities_path
+    end
+
+    private
+
+    def activity_params
+        params.require(:activity).permit(:name, :time_required)
+    end
 end
