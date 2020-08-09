77882e7ba8bf72e98bd6e388773eb2db73b85ff9
Emily Gann <aspentree79@gmail.com>
2020-08-09 11:15:38 -0400

    Created and built sessions_controller

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,24 @@
+class SessionsController < ApplicationController
+    def index
+    end
+
+    def new
+    end
+
+    def create
+        @user = User.find_by(username: params[:user][:username])
+        if @user && @user.authenticate(params[:user][:password])
+            session[:user_id] = @user.id
+            redirect_to root_path
+        else
+            flash[:message] = "Incorrect login info. Please try again."
+            redirect_to login_path
+        end
+    end
+
+    def destroy
+        session.clear
+        redirect_to root_path
+    end
+
+end
