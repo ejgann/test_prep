dcf29a4d9c78b659a520a02a03b9e2002c8a4e00
Emily Gann <aspentree79@gmail.com>
2020-08-09 11:20:29 -0400

    built tests_controller

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,2 +1,48 @@
 class TestsController < ApplicationController
+
+    def index
+        @tests = Test.all
+    end
+
+    def new
+        @test = Test.new
+    end
+
+    def create
+        @test = Test.new(test_params)
+        if @test.save
+            redirect_to tests_path
+        else
+            render :new
+        end
+    end
+
+    def show
+        @test = Test.find_by(id: params[:id])
+    end
+
+    def edit
+        @test = Test.find_by(id: params[:id])
+    end
+
+    def update
+        @test = Test.find_by(id: params[:id])
+        if @test.update(test_params)
+            redirect_to test_path(@test)
+        else
+            render :edit
+        end
+    end
+
+    def destroy
+        @test = Test.find_by(id: params[:id])
+        @test.destroy
+        redirect_to tests_path
+    end
+
+    private
+
+    def test_params
+        params.require(:test).permit(:name)
+    end
 end
