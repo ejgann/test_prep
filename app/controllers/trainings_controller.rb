751fca61a6f44c1d7f5299e8a2aa3b013b3f91eb
Emily Gann <aspentree79@gmail.com>
2020-08-10 07:51:28 -0400

    correcting logic in trainings controller to more accurately reflect parent activity relationship

--- app/controllers/trainings_controller.rb
+++ app/controllers/trainings_controller.rb
@@ -1,19 +1,29 @@
 class TrainingsController < ApplicationController
     def index
-        @trainings = Training.all
+         #    if the training is nested/associated with an activity, then trainings set equal to activity.trainings
+        if params[:activity_id] && Activity.find_by_id(params[:activity_id])
+            @trainings = @activity.trainings
+        else
+            @trainings = Training.all
+        end
+    
     end
 
     def new
+        # if it's nested under an activity AND the activity exists in the database, then training instance will be built in conjunction with that parent activity as part of has_many relationship
+        if params[:actvity_id] && Activity.find_by_id(params[:activity_id])
+            @training = @activity.trainings.build
+        # Otherwise, a new training created per usual
+        else
         @training = Training.new
+        # @training.build_activity
+        # belongs_to - nested form
+        end 
     end
 
     def create
-        @training = current_user.trainings.build(training_params)
-        if @training.save
-            redirect_to trainings_path
-        else
-            render :new
-        end
+        # if the training is nested/associated with an activity, then the training instance is set to be built with the parent activity
+        # otherwise, create a training instance as usual
     end
 
     def show
