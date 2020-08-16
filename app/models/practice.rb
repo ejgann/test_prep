9c9843052c9e737a6539d04787533cef6f027e03
Emily Gann <aspentree79@gmail.com>
2020-08-16 16:51:21 -0400

    Changed/added to practice and activities index views

--- app/models/practice.rb
+++ app/models/practice.rb
@@ -1,4 +1,7 @@
 class Practice < ApplicationRecord
   belongs_to :user
   belongs_to :activity
+
+  validates :date, presence: true
+  validates :comment, presence: true
 end
--- app/models/test.rb
+++ app/models/test.rb
@@ -1,3 +1,6 @@
 class Test < ApplicationRecord
   belongs_to :user
+
+  validates :name, presence: true
+  validates :date, presence: true 
 end
