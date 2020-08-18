08f9ae212623b276c48dd6e561d1d58399d4c5eb
Emily Gann <aspentree79@gmail.com>
2020-08-18 12:10:20 -0400

    fixed an issue with practice#show view; created spec.md file and pasted text from learn's repo file

--- app/models/test.rb
+++ app/models/test.rb
@@ -3,4 +3,13 @@ class Test < ApplicationRecord
 
   validates :name, presence: true
   validates :date, presence: true 
+
+  # scope :countdown, where("date > ?", (@test.date) - (Date.today).to_i)
+
+  # def self.countdown(date)
+  #   test_date = @test.date.to_i
+  #   today = Date.
+  # end
+
+
 end
